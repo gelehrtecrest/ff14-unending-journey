344676b938b8e9f8ee2e30ebc45f86edb4e9147c
Gelehrte Crest <ein.gelehrte+ff14@gmail.com>
2021-12-13 05:52:14 +0900

    少しずつ

--- docs/step1/image_j.bat
+++ docs/step1/image_j.bat
@@ -3,8 +3,8 @@
 rem 作業フォルダ
 set DIR=docs\step1
 
-rem 最初はテンプレート
-copy %DIR%\template_j.md %DIR%\index_j.md
+rem 初期化
+if exist %DIR%\index_j.md del %DIR%\index_j.md
 
 rem 残りは画像ファイル
 for %%f in (%DIR%\image_j\*) do (
