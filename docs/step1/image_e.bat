553508f26294fcfdca108d352ecac8591cdd4b7b
Gelehrte Crest <ein.gelehrte+ff14@gmail.com>
2021-12-13 12:46:59 +0900

    ファイルの移動とか軽量化とか

--- docs/step1/image_e.bat
+++ docs/step1/image_e.bat
@@ -2,12 +2,14 @@
 
 rem 作業フォルダ
 set DIR=docs\step1
+rem 対象
+set OUTPUT=index_e.md
 
 rem 初期化
-if exist %DIR%\index_e.md del %DIR%\index_e.md
+if exist %DIR%\%OUTPUT% del %DIR%\%OUTPUT%
 
 rem 残りは画像ファイル
 for %%f in (%DIR%\image_e\*) do (
     echo %%~nxf
-    echo ![%%~nxf]^(./image_e/%%~nxf^) >> %DIR%\index_e.md
+    echo [![%%~nxf]^(./image_e/%%~nxf^)]^(./image_e/%%~nxf^) >> %DIR%\index_e.md
 )
