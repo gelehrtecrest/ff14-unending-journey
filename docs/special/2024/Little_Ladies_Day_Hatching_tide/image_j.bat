aff4b4d79a8320cc1960185f5b2274785a9961bf
Gelehrte Crest <ein.gelehrte+ff14@gmail.com>
2024-04-05 23:24:01 +0900

    プリンセスデーエッグハント2024

---
+++ docs/special/2024/Little_Ladies_Day_Hatching_tide/image_j.bat
@@ -0,0 +1,22 @@
+@echo off
+
+rem 作業フォルダ
+set DIR=docs\special\2024\Little_Ladies_Day_Hatching_tide
+rem 画像フォルダ
+set IMG=image_j
+rem サムネイルフォルダ
+set IMG_THUMB=image_j_thumb
+rem 対象
+set OUTPUT=index_j.md
+
+rem 初期化
+if exist %DIR%\%OUTPUT% del %DIR%\%OUTPUT%
+if exist %DIR%\%IMG_THUMB% rd /s /q %DIR%\%IMG_THUMB%
+md %DIR%\%IMG_THUMB%
+
+rem 残りは画像ファイル
+for %%f in (%DIR%\%IMG%\*) do (
+    echo %%~nxf
+    magick convert -resize 480x270 %DIR%\%IMG%\%%~nxf %DIR%\%IMG_THUMB%\%%~nxf.thumb.jpg
+    echo [![%%~nxf]^(./%IMG_THUMB%/%%~nxf.thumb.jpg^)]^(./%IMG%/%%~nxf^) >> %DIR%\%OUTPUT%
+)
